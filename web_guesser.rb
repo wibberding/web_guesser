3f06c091c74690ca0c0b41098edd1156bacdaf45
Lonnie <wibberding@gmail.com>
2015-04-24 09:33:31 -0700

    Working number guessing game."
    exit

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,23 +4,34 @@ require 'sinatra/reloader'
 
 #Declared here so it doesn't change on refresh.
 @@secret_number = rand(101) # 101 to include the number 100.
-@@guesses_left = 5
+@@guesses_left = 5 +1 # "1" is to account for first page load.
 
 
 get '/' do
   guess = params["guess"]
-  @@guesses_left -= 1 if guess !=""  #counts guesses and avoids changing on blank input.
-  message = check_guess(guess)
+  cheat = params["cheat"]
 
-  if @@guesses_left <= 0 
-    message = "You lost. I new number has been generated."
+  background_color = change_color(guess, @@secret_number) # intentional dependency injection. Is this the 'right way' to make the code clear, or is it better to just reference the class variable directly like check_guess()? Perhaps referencing a class variable is okay since you don't change it and there are no side effects??
+
+  @@guesses_left -= 1 if guess !=""  # counts guesses and avoids changing on blank input.
+  
+  if @@guesses_left >= 5
+    message = "Pick a number between 0 and 100."
+  else
+    message = check_guess(guess)
+  end
+  
+  reset() if guess.to_i == @@secret_number # resets on correct guess.
+
+  if @@guesses_left <= 0 # resets and displays message on end of game.
+    message = "You lost. The number was #{@@secret_number.to_s}. A new number has been generated."
     reset()
   end
 
-  background_color = change_color(guess, @@secret_number) # intentional dependency injection. Is this the 'right way' to make the code clear, or is it better to just reference the class variable directly like check_guess()? Perhaps referencing a class variable is okay since you don't change it and there are no side effects??
-  erb :index, :locals => {:number => @@secret_number.to_s, :message => message, :background_color => background_color, :guesses => @@guesses_left}
-
+  erb :index, :locals => {:number => @@secret_number.to_s, :guess => guess.to_s, :message => message, :background_color => background_color, :guesses => @@guesses_left, :cheat => cheat}
 end
+
+
 
 def check_guess(guess)
   guess = guess.to_i
@@ -34,7 +45,6 @@ def check_guess(guess)
     message = "Too low!"
   else
     message = "You got it right! A new number has been generated"
-    reset()
   end
   message
 end
@@ -46,13 +56,14 @@ def change_color(guess, number)
     return "green"
   end
   
-  offset = 7.1 # number of color steps 
+  offset = 7.1 # color steps offset.
   color_number = (13 - (spread / offset).floor)
-  color_number = "a" if color_number == 10
-  color_number = "b" if color_number == 11
-  color_number = "c" if color_number == 12
-  color_number = "d" if color_number == 13
-  color_number.to_s
+  # format for hex values.
+    color_number = "a" if color_number == 10
+    color_number = "b" if color_number == 11
+    color_number = "c" if color_number == 12
+    color_number = "d" if color_number == 13
+    color_number.to_s
 
   return "#f#{color_number}#{color_number}"
 end
